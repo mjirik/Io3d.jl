21c35236b485688261907d87f2bb49c0fd996da3
Miroslav Jirik <miroslav.jirik@gmail.com>
2019-04-03 11:16:34 +0200

    added function for list available labels

--- src/Io3d.jl
+++ src/Io3d.jl
@@ -1,13 +1,15 @@
 module Io3d
+using PyCall
 
 greet() = print("Hello World!")
 
-using PyCall
 
 
+"""
+Read 3D data from file. Data are stored int dictionary with keys "data3d" and "voxelsize_mm".
+"""
 function read3d(fn)
     pyio3d = pyimport("io3d")
-
     return pyio3d["read"](fn);
 end
 
@@ -17,6 +19,34 @@ function datasets_join_path(pth)
     opth = pyio3d_datasets["join_path"](pth, get_root=true)
     print(opth)
     return opth
+end
+
+"""
+Download test data based on input label. Available labels can be list by get_labels()
+"""
+function datasets_download(label)
+    pyio3d_datasets = pyimport("io3d.datasets")
+#     print(pth)
+    pyio3d_datasets["download"](label)
+end
+
+"""
+Write data into file. Data have to be in dict with labels "data3d" and "voxelsize_mm".
+"""
+function write3d(arr, fn)
+    pyio3d = pyimport("io3d")
+    return pyio3d["write"](arr, fn);
+#     print(pth)
+end
+
+
+"""
+Get list of available datasets labels.
+"""
+function get_labels()
+    pyio3d_datasets = pyimport("io3d.datasets")
+    labels = pyio3d_datasets["get_labels"]()
+    return labels
 end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,15 @@ using Io3d
 using Test
 
 @testset "Io3d.jl" begin
+    labels = Io3d.get_labels()
+
+    Io3d.datasets_download("nrn4")
+    pth = Io3d.datasets_join_path("medical/orig/sample-data/nrn4.pklz")
+    @test length(pth) > 0
+    datap = Io3d.read3d(pth)
+    data3d = datap["data3d"]
+    @test eltype(data3d) == UInt16
+
+    Io3d.write3d(data3d, "test_output_nrn4.dcm")
     # Write your own tests here.
 end
